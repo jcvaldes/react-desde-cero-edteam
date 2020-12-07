9225bf05312ca658aaf757f6c9f1aeb472336e4c
Juan Carlos Valdes <idevkingos@gmail.com>
2020-12-07 18:48:40 -0300

    useState hook

--- src/components/Pages/Course.jsx
+++ src/components/Pages/Course.jsx
@@ -1,55 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-const cursos = [
-  {
-    id: 1,
-    titulo: 'Consumiendo APIs con Blazor',
-    image:
-      'https://edteam-media.s3.amazonaws.com/courses/medium/d438e9fa-77f0-4184-9d70-dd7778c309ec.png',
-    price: 70,
-    profesor: 'Beto Quiroga',
-  },
-  {
-    id: 2,
-    titulo: 'Scripting - Bash / Shell',
-    image:
-      'https://edteam-media.s3.amazonaws.com/courses/medium/bdf0b692-eef2-411b-8502-56d1d93fa85d.png',
-    price: 40,
-    profesor: 'Diego Blas',
-  },
-  {
-    id: 3,
-    titulo: 'Consumiendo APIs con Blazor',
-    image:
-      'https://edteam-media.s3.amazonaws.com/courses/medium/d438e9fa-77f0-4184-9d70-dd7778c309ec.png',
-    price: 20,
-    profesor: 'Beto Quiroga',
-  },
-  {
-    id: 4,
-    titulo: 'Consumiendo APIs con Blazor',
-    image:
-      'https://edteam-media.s3.amazonaws.com/courses/medium/d438e9fa-77f0-4184-9d70-dd7778c309ec.png',
-    price: 50,
-    profesor: 'Beto Quiroga',
-  },
-];
-const Course = ({ match }) => {
-  const cursoActual = cursos.find(c => c.id === parseInt(match.params.id));
+import React, { useState } from 'react';
 
+const Course = ({ match }) => {
+  const [state, setState] = useState({
+    course: {
+      "id": 1,
+      "title": "React desde Cero",
+      "image": "https://edteam-media.s3.amazonaws.com/courses/medium/d438e9fa-77f0-4184-9d70-dd7778c309ec.png",
+      "price": 44,
+      "teacher": "Juan Carlos"
+    }
+  })
+  const { title, teacher, image } = state.course;
   return (
     <div className="ed-grid m-grid-3">
       {
-        cursoActual 
+        title 
           ? (
             <>
-            <h1 className="m-cols-3">{cursoActual.titulo}</h1>
+            <h1 className="m-cols-3">{title}</h1>
             <img
               className="m-cols-1"
-              src={cursoActual.image}
-              alt={cursoActual.titulo}
+              src={image}
+              alt={title}
             />
-            <p className="m-cols-2">Profesor: {cursoActual.profesor}</p>
+            <p className="m-cols-2">Profesor: {teacher}</p>
             </>
           )
           : <h1>Course no existe</h1>
